b97c54e188432009e28a0e31f0321d338e1c4dd0
Martin Schmidt <martin@scoding.dk>
2023-10-09 12:13:59 +0000

    added test for FromSeed

--- src/ProjectOrigin.HierarchicalDeterministicKeys.Tests/Secp256k1Tests.cs
+++ src/ProjectOrigin.HierarchicalDeterministicKeys.Tests/Secp256k1Tests.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using AutoFixture;
 using FluentAssertions;
 using ProjectOrigin.HierarchicalDeterministicKeys;
@@ -37,5 +38,21 @@ public class Secp256k1Tests
         var verifcationResult = key.PublicKey.Verify(data, signature);
 
         verifcationResult.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Secp256k1_from_seed()
+    {
+        var data = _fixture.Create<byte[]>();
+
+        var bytes = Encoding.UTF8.GetBytes("my seed hello world");
+
+        var key1 = Algorithms.Secp256k1.CreateFromSeed(bytes);
+        var signature1 = key1.Sign(data).ToArray();
+
+        var key2 = Algorithms.Secp256k1.CreateFromSeed(bytes);
+        var signature2 = key2.Sign(data).ToArray();
+
+        signature1.Should().BeEquivalentTo(signature2);
     }
 }
